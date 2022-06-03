a68c1a0605cd466ddeea059ce16dd3f92e7004a8
Abdullah <54276938+Abdul535@users.noreply.github.com>
2022-06-04 01:39:51 +0530

    AxesHelper, OrbitControls and performance++

--- src/script.js
+++ src/script.js
@@ -1,10 +1,15 @@
 import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 //scene
 const scene = new THREE.Scene();
 
-//objects : include geometry,material and mesh
+//Axes Helper
+const AxesHelper = new THREE.AxesHelper(5)
+scene.add(AxesHelper)
+
+//Objects : include geometry,material and mesh
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 //Mesh args (geometry, material)
@@ -13,30 +18,75 @@ scene.add(cube);
 
 //camera
 const sizes = {
-    width:1000,
-    height:600
+    width:window.innerWidth,
+    height:window.innerHeight
 }
+
 // PerspectiveCamera args (fov, aspectRatio, near, far)
 const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 1, 1000)
 camera.position.z = 5;
-cube.rotation.x = 0;
-cube.rotation.y = 0;
 scene.add(camera)
 
 //renderer
 const canvas = document.querySelector('.webgl');
 console.log(canvas)
 const renderer = new THREE.WebGLRenderer({
-    canvas:canvas 
+    canvas
 })
+
+///////////////////////////////
+//resize,update logic
+window.addEventListener('resize',()=>{
+
+    //update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    //update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    renderer.setSize(sizes.width,sizes.height)
+
+})
+
 renderer.setSize(sizes.width,sizes.height)
-renderer.render(scene,camera)
 
-//we can't see any thing bcoz camera and cube is at center and we can see only from outside of object , if a camera is inside an object there is nothing its all black
-//need to backoff the camera
-//to transform an object we can use -position -rotation -scale
-// position property takes x,y and z axis
-//y is always UP, x is always rightside and z is forward/backward always in THREE.js
-//changing camera position on line 18
+//pixel ratio either 2 aur less than 2
+renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
 
 
+////////////////////////////////////////
+//fullscreen dblclick logic
+window.addEventListener('dblclick',()=>{
+    if(!document.fullscreenElement){
+        canvas.requestFullscreen()
+    }
+    else{
+        document.exitFullscreen()
+    }
+})
+
+
+////////////////////////////////////////////
+//using OrbitControls
+const controls = new OrbitControls(camera, canvas)
+controls.target.y = 1
+controls.enableDamping = true
+controls.enabled = true
+
+
+const clock = new THREE.Clock();
+
+////////////////////////////////////////////////
+//requestAnimationFrame
+const loop = () =>{
+
+    const eTime = clock.getElapsedTime();
+    controls.update()
+
+    renderer.render(scene,camera)
+    window.requestAnimationFrame(loop)
+}
+
+loop()
